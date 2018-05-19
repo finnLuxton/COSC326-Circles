602f90797573b64e8da7c6928c522e01cc82aac2
Finlay Luxton <luxfi448@student.otago.ac.nz>
2018-05-19 21:43:21 +1200

    Updated with random ideas.

--- Circles.java
+++ Circles.java
@@ -9,19 +9,38 @@ public class Circles extends JFrame{
     
     public Circles() {
         setSize(800,800);
-        setLocation(100,100);
+        setLocationRelativeTo(null);
         setVisible(true);
     }
 
-
     public void paint(Graphics g){
-        int x = 50;
-        int y = 50;
-        g.setColor(Color.yellow);
-        g.fillOval(x,y,100,100);
-        g.fillOval(x+50,y+50,100,100);
+        // Write some pseudocode down on paper whydontcha?
+        
+        // Clears it, right?
+        super.paint(g);
 
+        // Getting dimensions of window
+        int width = getWidth();
+        int height = getHeight();
+        System.out.println("Width: " + width + " Height: " + height);
+        // Getting center of window
+        int centWidth = width / 2;
+        int centHeight = height / 2;
+        System.out.println("Center of screen = " + centWidth + "," + centHeight);
+        
+        // Make sure that color is the same for each generation
+        g.setColor(Color);
+        for(i = 0; i < 2; i++){
+            circleCount = 
+            drawCircle(g, centWidth, centHeight);
+        }
     }
 
+    public void drawCircle(Graphics g, int x, int y){
+        //x, y, width, height
+        //g.fillOval(x/2, y/2, 400, 400);
+
+        g.fillOval(x/2, y/2, 400, 400);
+    }
 
 } 
