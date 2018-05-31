a9376ba3881ed4ea45428ac209112d9adfd1359c
Finlay Luxton <luxfi448@student.otago.ac.nz>
2018-05-31 17:54:03 +1200

    Final Submission Takes in file input under -file flag

--- Circles.py
+++ Circles.py
@@ -6,31 +6,36 @@ import matplotlib.pyplot as plt
 from fractions import Fraction
 from argparse import ArgumentParser
 
+# COSC326 Etude 13 - Circle
+# Author - Finn Luxton 6362897
+
 generations = 3
 circles = []
 colorList = []
 ratio = []
 inputColor = []
 
+# Creates the main parent circle which will encompass all other inner circles
 def create_parent(parentRadius):
     global colorList, circles
     circle = plt.Circle((0, 0), radius = parentRadius, color=colorList[0])
     circles.append(circle)
 
+# Creates an array of random colors if no color input is given
 def create_colors():
     global colorList, generations
-
     i = 0
     while i < generations:
         colorList.append([random.uniform(0, 1.0), random.uniform(0, 1.0), random.uniform(0, 1.0)])
         i = i + 1
 
+# Creates a cluster of circles inside a parent circle
 def create_cluster(subX, subY, parentRadius, printedGenerations):
-    global colorList, clusterFlag, circles
+    global colorList, circles
 
     if printedGenerations == generations:   
         return None
-        
+
     # Center
     circleC = plt.Circle((subX,subY), radius = parentRadius, color=colorList[printedGenerations])
     # Left
@@ -46,77 +51,75 @@ def create_cluster(subX, subY, parentRadius, printedGenerations):
     # Bottom Right
     circleBR = plt.Circle((subX+(parentRadius), subY - (int((1.75*parentRadius)))), radius = parentRadius, color=colorList[printedGenerations])
 
-    circles.append(circleC)
-    circles.append(circleL)
-    circles.append(circleR)
-    circles.append(circleTL)
-    circles.append(circleTR)
-    circles.append(circleBL)
-    circles.append(circleBR)
+    circles.extend([circleC, circleL, circleR, circleTL, circleTR, circleBL, circleBR])
 
     if printedGenerations < generations:
         # Left
-        create_cluster(subX-(2*parentRadius), subY, parentRadius / 3, printedGenerations + 1)
+        create_cluster(subX-(2*parentRadius), subY, parentRadius * 0.333, printedGenerations + 1)
         # Center
-        create_cluster(subX, subY, parentRadius / 3, printedGenerations + 1)
-
+        create_cluster(subX, subY, parentRadius * 0.333, printedGenerations + 1)
         # Right
-        create_cluster(subX+(2*parentRadius), subY, parentRadius / 3, printedGenerations + 1)
+        create_cluster(subX+(2*parentRadius), subY, parentRadius * 0.333, printedGenerations + 1)
         # Top Left
-        create_cluster(subX-(parentRadius), subY + (int((1.75*parentRadius))), parentRadius / 3, printedGenerations + 1)
+        create_cluster(subX-(parentRadius), subY + (int((1.75*parentRadius))), parentRadius * 0.333, printedGenerations + 1)
         # Top Right
-        create_cluster(subX+(parentRadius), subY + (int((1.75*parentRadius))), parentRadius / 3, printedGenerations + 1)
+        create_cluster(subX+(parentRadius), subY + (int((1.75*parentRadius))), parentRadius * 0.333, printedGenerations + 1)
         # Bottom Left
-        create_cluster(subX-(parentRadius), subY - (int((1.75*parentRadius))), parentRadius / 3, printedGenerations + 1)
+        create_cluster(subX-(parentRadius), subY - (int((1.75*parentRadius))), parentRadius * 0.333, printedGenerations + 1)
         # Bottom Right
-        create_cluster(subX+(parentRadius), subY - (int((1.75*parentRadius))), parentRadius / 3, printedGenerations + 1)
+        create_cluster(subX+(parentRadius), subY - (int((1.75*parentRadius))), parentRadius * 0.333, printedGenerations + 1)
 
     return
 
-# Make a list of circles, and add in for loop using add_artist(i)
-def show_shape():
-    global circles
-    graph_size = 300
-    fig, ax = plt.subplots()
-    i = 0
-    for i in circles:
-        ax.add_artist(i)
-
-    plt.axis('scaled')
-    plt.axis([-graph_size, graph_size, -graph_size, graph_size])
-    plt.axis('off')
-    plt.show()
-
 if __name__=='__main__':
-
+    printedGenerations = 0
+    # Closes any still open plots
     plt.close()
-    ## if len(argv) == 2
 
     argument = ArgumentParser()
     argument.add_argument('-file', metavar='FILE', help='input file for Circles. <Ratio> <R> <G> <B>')
     args = argument.parse_args()
-    
+    lines = []
 
     if(args.file):
         with open(args.file) as file:
-            lines = []
             for line in open(args.file):
                 line = shlex.split(line)
                 lines.append(line)
-                
-    # what the fuuuuuck, why isnt this running correctly? Thinks i is an Integer and not an int???
+    # 
+    if len(lines) > 0:
+        generations = len(lines)
+
     i = 0
-    for i in lines:
-        if i == 1:
-            ratio = float(Fraction(lines[1][0]))
-    print ratio
-    #
-    create_colors()
+    ratio = []
+    for i in lines: 
+        ratio.append( float( Fraction(i[0])))
+        colorList.append([int(i[1])/255, int(i[2])/255, int(i[3])/255])
+        
+    if(not args.file):
+        create_colors()
 
     if(generations > 1):
-        create_parent(300 * ratio)
+        if len(lines) > 0:
+            create_parent(300 * ratio[printedGenerations])
+        else:
+            create_parent(300)
+        printedGenerations += 1
     if(generations >= 2):
-        create_cluster(0, 0, 300/3, 1)
+        if len(lines) > 0:
+            create_cluster(0, 0, 300 * ratio[printedGenerations], printedGenerations)
+        else:
+            create_cluster(0, 0, 300 * 0.333, printedGenerations)
 
-    show_shape()
+    graph_size = 300
+    fig, ax = plt.subplots()
+    c = 0
+    for c in circles:
+        ax.add_artist(c)
+  
+    plt.axis('scaled')
+    plt.axis([-graph_size, graph_size, -graph_size, graph_size])
+    plt.axis('off')
+
+    plt.show()
 
---
+++ ReadMe.txt
@@ -0,0 +1,9 @@
+To run the program with defaults, use the following command
+
+./Circles.py
+
+To run the program with a file input, use the following command, adding your file between the chevrons
+
+/Circles.py -file <FILENAME>
+
+If the file cannot be run, there may be a chance that you do not have permissions. Use chmod to fix this.
